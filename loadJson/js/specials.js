88984d54eaa63c9176894b2cd386f6e9c1d2e967
ankit.bansal001@yahoo.comclear <root@vinsol-dell-1525-silver.(none)>
2013-07-19 18:30:41 +0530

    loadJson

---
+++ loadJson/js/specials.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+  $('#specials').append('<div id="outputs"></div>');
+  retrieveJson();
+})
+//function to show the div part on clicking the option..
+function retrieveJson(){
+  var jsonObj = {};
+  $.ajax({
+    url: "data/specials.json",
+    dataType: 'json',
+    success: function(data){
+      jsonObj = data;
+    }
+  })
+  $('#specials select').bind("change", function(e){
+    var input = $(this).val();
+    $('li.buttons').remove();
+    var key = jsonObj[input];
+    $('#outputs').html(key.title + "<p>" + key.text + "</p>");
+    $('#outputs').attr('style', 'color:' + key.color);
+    $('#outputs').append('<p><img src="../loadJson' + key.image + '" /></p>')
+  })
+}
