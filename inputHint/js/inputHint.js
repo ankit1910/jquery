f7cd3e8b12b88e74efe9e0add3814ef7a58fd30f
ankit.bansal001@yahoo.comclear <root@vinsol-dell-1525-silver.(none)>
2013-07-20 13:22:56 +0530

    input hint

--- inputHint/js/inputHint.js
+++ inputHint/js/inputHint.js
@@ -1,14 +1,19 @@
-var box = $('#q');
-box.val("Enter search term");
-box.addClass("hint");
-$('label[for="q"]').remove();
-//binding focus and blur event to a single function..
-box.bind('focus blur', function(e){
-  $(this).toggleClass('hint');
-  if(e.type == 'focus'){
-    $(this).val("");
-  }
-  else if(e.type == 'blur' && !($(this).val()) ){
-    $(this).val("Enter search term");
-  }
+$(function(){
+  var textHint = $('label[for="q"]').text();
+  $('label[for="q"]').remove();
+  $('#q') .addClass("hint")
+          .val(textHint)
+          .bind('focus', function(){
+            $(this).addClass('hint');
+            if($(this).val() == textHint){
+              $(this).val("");
+            }
+          })
+          .bind('blur',function(){
+            if($(this).val() == ""){
+              $(this).val(textHint);
+            }else{
+              $(this).removeClass('hint');
+            }
+          })
 })
