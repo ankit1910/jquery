9015279a535055d4851810e8219a33b303f19629
ankit.bansal001@yahoo.comclear <root@vinsol-dell-1525-silver.(none)>
2013-07-22 14:45:18 +0530

    manupulating

--- manupulating/js/sandbox.js
+++ manupulating/js/sandbox.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   //1. Add five new list items to the end of the unordered list #myList.
-  var itemToAdd = 5
+  var itemToAdd = 5;
   var myList = $('#myList');
   for(var i = 1; i <= itemToAdd ; i++){
     myList.append("<li>Appended item'" + i + "'</li>");
@@ -8,14 +8,13 @@ $(document).ready(function(){
   //2. Remove the odd list items
   $('#myList li:odd').remove();
   //3. Add another h2 and another paragraph to the last div.module
-  divHeading = "heading tag appended";
-  divParagraph = "paragraph tag appended";
-  $('div.module:last').prepend("<h2>" + divHeading + "</h2><p>" + divParagraph + "</p>");
+  var divHeading = "heading tag appended", divParagraph = "paragraph tag appended";
+  $('div.module:last').append("<h2>" + divHeading + "</h2><p>" + divParagraph + "</p>");
   //4. Add another option to the select element; give the option the value "Wednesday"
-  optionValue = "Wednesday";
+  var optionValue = "Wednesday";
   $('select[name="day"]').append("<option value='" + optionValue +"'>Wednesday</option>")
   //5.Add a new div.module to the page after the last one; put a copy of one of the existing images inside of it.
-  var imageSrc = $("img[alt]").attr("src");
+  var imageSrc = $("img[alt='fruit']").clone();
   $("<div class='module'></div>").insertAfter($('div.module:last'))
-                                 .append('<img src="' + imageSrc + '" />');
-})
+                                 .append(imageSrc);
+});
