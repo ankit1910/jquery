2d7bb9f3a0121304a067d524e6eb6a0108c487d3
ankit.bansal001@yahoo.comclear <root@vinsol-dell-1525-silver.(none)>
2013-07-23 14:58:53 +0530

    liveDelegate

--- liveDelegate/liveDelegate.js
+++ liveDelegate/liveDelegate.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
   var count = 1;
   $('#add').click(function(){
-    $('#container').append('<div id="innerdiv' + count + '"></div>');
-    $('#innerdiv' + count +'').text("div no" + (count++) + "")
-                              .attr('style', 'border: solid 1px; height: 60px');
-    })
-  $('#container').delegate('div', 'click', function(){
+    $('#container').append('<div alt="removable" id="innerdiv' + count + '"></div>')
+                   .find('#innerdiv' + count +'').text("div no" + (count++) + "")
+                                                 .attr('style', 'border: solid 1px; height: 60px');
+  })
+  $('#container').delegate('div[alt="removable"]', 'click', function(){
     if($(this).is(':last-child')){
       $(this).remove();
       count--;
