3bb706f59b8b1f1c8e9da5fb51fdcd2e6fdbdf0b
ankit.bansal001@yahoo.comclear <root@vinsol-dell-1525-silver.(none)>
2013-07-23 15:33:56 +0530

    revealHiddenText

--- revealHiddenText/js/blog.js
+++ revealHiddenText/js/blog.js
@@ -1,11 +1,8 @@
 $(document).ready(function(){
-  clickBlog();
-})
-//function to slide down the blog content..
-function clickBlog(){
-  $('#blog a').bind('click', function(){
-    $('p.excerpt').slideUp(0);
-    $(this).attr('href', 'javascript:void(0)');
+  $('#blog').delegate('a', 'click', function(e){
+    e.preventDefault();
+    $('p.excerpt').slideUp('slow');
     $(this).parent().next().slideDown('slow');
-  })
-}
+  });
+});
+
