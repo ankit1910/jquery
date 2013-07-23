ea4a569619e36f2304cc156c5e61f86284349a96
ankit.bansal001@yahoo.comclear <root@vinsol-dell-1525-silver.(none)>
2013-07-23 13:19:04 +0530

    traversing

--- traversing/js/sandbox.js
+++ traversing/js/sandbox.js
@@ -1,26 +1,21 @@
-$(document).ready(function() {
+$(function(){
   // 1. Select all of the image elements on the page; log each image's alt attribute.
-  var disp = "";
   $('img').each(function(){
-    disp += this.alt;
-    disp += "\n";
+    var disp = $(this).attr('alt');
+    console.log(disp)
   })
-  alert("image's alt attribute\n" + disp);
   // 2. Select the search input text box, then traverse up to the form and add a class to the form.
-  alert("form html\n" + $('input.input_text').parent().html());
+  $('#q').parent().addClass('hint');
+  console.log("class added label color changed to grey");
   // 3. Select the list item inside #myList that has a class of "current" and remove that class from it; add a class of "current" to the next list item.
-  $('#myList li').each(function(){
-    if($(this).hasClass("current")){
-      $(this).removeClass("current");
-      $(this).next().addClass("current");
-      return false;
-    }
-  })
-  alert("class has changed");
+  $('li.current').removeClass("current")
+                 .next().addClass("current");
+  console.log("class has changed");
   // 4. Select the select element inside #specials; traverse your way to the submit button.
-  alert("traversing to the submit\n" + $('#specials select[name="day"]').parent().next().children().val());
+  var valueButton = $('#specials select:first').parent().next().children().val();
+  console.log("traversed to the submit button its value is " + valueButton);
   // 5. Select the first list item in the #slideshow element; add the class "current" to it, and then add a class of "disabled" to its sibling elements.
-  $('#slideshow li:first-child').addClass("current");
-  $('#slideshow li:not(:first-child)').addClass("disabled");
-  alert("class has changed");
+  $('#slideshow li:first-child').addClass("current")
+                                .siblings().addClass("disabled");
+  console.log("class has changed");
 })
