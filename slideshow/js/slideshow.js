4b26d295efec41828642200fad7b289ca29fa248
ankit.bansal001@yahoo.comclear <root@vinsol-dell-1525-silver.(none)>
2013-07-24 11:19:38 +0530

    slideshow

--- slideshow/js/slideshow.js
+++ slideshow/js/slideshow.js
@@ -1,13 +1,13 @@
-var slideshow = $('#slideshow');
-var len = slideshow.children("li").length, i = 0;
-$(document).ready(function(){
-  $('body').prepend(slideshow);
-  slideshow.children('li').hide();
-  $(slideshow).after("<div id='description' class='current'></div>");
-  fadeInOut();
+$(function(){
+  var $slideshow = $('#slideshow');
+  var len = $slideshow.children("li").length, i = 0;
+  $('body').prepend($slideshow);
+  $slideshow.children('li').hide()
+                           .parent().after("<div id='description' class='current'></div>");
+  fadeInOut(len, i);
 })
 //function to display the slide show..
-function fadeInOut(){
+function fadeInOut(len, i){
   var image = $('#slideshow li:eq('+ (i++) +')');
   $('#description').html("(there are total '" + len + "' images: )" + "\n" + " And you are viewing '" + image.children("h2").text() + "'.");
   image.fadeIn(1000).delay(1000).fadeOut(1000);
@@ -15,5 +15,5 @@ function fadeInOut(){
     i = 0;
   }
   //repeating the next image after particular time..
-  setTimeout(fadeInOut,3000);
+  setTimeout(function(){fadeInOut(len,i)},3000);
 }
