09c6152760a9a288404b628971166eb348ff6805
ankit.bansal001@yahoo.comclear <root@vinsol-dell-1525-silver.(none)>
2013-07-26 12:20:32 +0530

    load json

--- loadJson/js/specials.js
+++ loadJson/js/specials.js
@@ -2,26 +2,32 @@ $(function(){
   $('#specials').append('<div id="outputs"></div>');
   $('li.buttons').remove();
   var jsonObj = {};
-  $('#specials select').one("mousedown", function(){
+  $('#specials select').one("change", function(){
+    var that = $(this);
     $.ajax({
       url: "data/specials.json",
       dataType: 'json',
       success: function(data){
         jsonObj = data;
-        dispJson(jsonObj);
+        var input = that.val();
+        dispJson(jsonObj, input);
       }
     })
-  })  
-});
-//function to show the div part on clicking the option..
-var dispJson = function(jsonObj){
-  $('#specials select').bind("mouseup", function(e){
-    var input = $(this).val();
-    if(input){   
-      var key = jsonObj[input];
-      $('#outputs').html(key.title + "<p>" + key.text + "</p>")
-                   .attr('style', 'color:' + key.color)
-                   .append('<p><img src="../loadJson' + key.image + '" /></p>');
-    }
+    $('#specials select').bind("change", function(e){
+      var input = $(this).val();
+      dispJson(jsonObj, input);
+    })
   })
+})
+//function to show the div part on clicking the option..
+var dispJson = function(jsonObj, input){
+  if(input){
+    var key = jsonObj[input];
+    $('#outputs').html(key.title + "<p>" + key.text + "</p>")
+                 .attr('style', 'color:' + key.color)
+                 .append('<p><img src="../loadJson' + key.image + '" /></p>');
+  }
+  else{
+    $('#outputs').html("");
+  }
 }
