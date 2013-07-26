718dd55adb5021b88ad58a72d3d1241608d10706
ankit.bansal001@yahoo.comclear <root@vinsol-dell-1525-silver.(none)>
2013-07-26 18:25:47 +0530

    dragAndDrop

--- dragAndDrop/libs/dragAndDrop.js
+++ dragAndDrop/libs/dragAndDrop.js
@@ -1,12 +1,39 @@
 $(function(){
-  //making list item with class 'item' dragable 
-  $(".item").draggable({helper: 'clone'});
+  //making list item with class 'item' dragable
+  $(".item").draggable({
+    helper: 'clone',
+    start: function(){
+      $(this).closest('div').addClass('highlight');
+    },
+    drag: function(){
+      $(this).addClass('selected');
+    },
+    stop: function(){
+      $('#second_box, #first_box').removeClass('notAccept');
+      $(this).removeClass('selected')
+             .closest('div').removeClass('highlight');
+    }
+  });
   //making first and second box dropable..
   $("#second_box, #first_box").droppable({
-    accept: '.item',
+    out: function(){
+      $(this).removeClass('highlight notAccept');
+    },
+    over: function(even, ui){
+      var itemDrop = $(ui.draggable);
+      if(itemDrop.hasClass('dropable')){
+        $(this).addClass('highlight');
+      }
+      else{
+        $(this).addClass('notAccept');
+      }
+    },
     drop: function(event, ui){
       var itemDrop = $(ui.draggable);
-      $(this).find('ul').append(itemDrop);
+      if(itemDrop.hasClass('dropable')){
+        $(this).removeClass('highlight')
+               .find('ul').append(itemDrop);
+      }
     }
   });
 });
