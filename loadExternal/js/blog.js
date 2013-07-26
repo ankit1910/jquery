623e46774eb775a099c1f32953c949280e5c3d43
ankit.bansal001@yahoo.comclear <root@vinsol-dell-1525-silver.(none)>
2013-07-26 12:54:06 +0530

    load external

--- loadExternal/js/blog.js
+++ loadExternal/js/blog.js
@@ -1,8 +1,9 @@
 $(document).ready(function(){
   var blog =  $('#blog h3');
   blog.each(function(index){
-  	$(this).after('<div class="blog" id="blog'+ index +'"></div>')
-  	       .data("divId",'blog'+index);
+    var divId = 'blog'+index ;
+  	$(this).after('<div class="blog" id="' + divId + '"></div>')
+  	       .data("divId",divId);
   	})
   showBlog(blog);
 })
@@ -10,8 +11,7 @@ $(document).ready(function(){
 var showBlog = function(blog){
   blog.bind("click", function(e){
     e.preventDefault();
-    var refrence = ($(this).children('a').attr('href')).split('');
-    var refrence = refrence[refrence.length-1];
-    $('#' + $(this).data("divId")).load('data/blog.html #post' + refrence );
+    var refrence = $(this).children('a').attr('href').replace('#', ' #');
+    $('#' + $(this).data("divId")).load('data/'+ refrence );
   })
 }
