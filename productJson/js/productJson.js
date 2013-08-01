8fedf0590a0248c9944ea66f00873fe7b82b0d4b
ankit.bansal001@yahoo.comclear <root@vinsol-dell-1525-silver.(none)>
2013-08-01 17:15:23 +0530

    productJson

--- productJson/js/productJson.js
+++ productJson/js/productJson.js
@@ -1,47 +1,37 @@
-$(document).ready(function(){
+$(function(){
   var jsonObj = [];
-  var filterClassArray = ['.brand', '.color', '.available'];
+  var filters = ['brand', 'color', 'sold_out'];
   $.ajax({
     url: "data/productJson.json",
     dataType: 'json',
     success: function(data){
       jsonObj = data;
-      var filter = new JSONfilters();
-      filter.displayBlocks(jsonObj);
-      $('.brand, .color, .available').bind('change', function(){
-        var blocks = $('#products').find('img');
-        filter.Filter(blocks, filterClassArray);
+      displayBlocks(jsonObj);
+      $('.brand, .color, .sold_out').bind('change', function(){
+        var blocks = $('#products img');
+        blocks.show();
+        for (var key in filters){
+          blocks = Filter(blocks, filters[key]);
+        }
       })
     }
   })
-});
-function JSONfilters(){
-  this.Filter = function(blocks, filterClassArray){
-    for(var i = 0; i < filterClassArray.length; i++){
-      $(blocks).hide();
-      if($('' + filterClassArray[i] + ':checked').length == 0){
-        $(blocks).show();
+})
+var Filter = function(blocks, filterType){
+  if($('.' + filterType + ':checked').length != 0){
+    blocks.hide();
+    $('.' + filterType + '').each(function(){
+      if($(this).prop('checked')){
+        var value = $(this).val();
+        blocks.filter('img[' + filterType + '="' + value + '"]').show();
       }
-      else{
-        $(filterClassArray[i]).each( function(){
-          var that = this;
-          if(($(that).prop('checked'))){
-            $(blocks).each( function(){
-              if($(that).attr('id') == $(this).data('filterData').brand || $(that).attr('id') == $(this).data('filterData').color || $(that).attr('id') == $(this).data('filterData').sold_out){
-                $(this).show();
-              }
-            })
-          }
-        })
-      }
-      blocks = $(blocks).filter(':visible');
-    }
+    })
   }
-  this.displayBlocks = function(jsonObj){
-    $('#products').empty();
-    for(var i = 0; i < jsonObj.length; i++){
-      $('#products').append('<img id ="img_' + i + '" class="prod" src="images/' + jsonObj[i].url + '" />');
-      $('#img_' + i + '').data("filterData", { color : jsonObj[i].color, brand : jsonObj[i].brand, sold_out : jsonObj[i].sold_out});
-    }
+  return blocks.filter(':visible');
+}
+var displayBlocks = function(jsonObj){
+  for(var i = 0; i < jsonObj.length; i++){
+    $('#products').append('<img id ="img_' + i + '" class="prod" src="images/' + jsonObj[i].url + '" />');
+    $('#img_' + i + '').attr({ color : jsonObj[i].color, brand : jsonObj[i].brand, sold_out : jsonObj[i].sold_out});
   }
 }
