7e829756a187e784db563eb3c0ec4190b6025ccb
ankit.bansal001@yahoo.comclear <root@vinsol-dell-1525-silver.(none)>
2013-08-01 15:39:35 +0530

    watchkart

--- watchkart/libs/watchkart.js
+++ watchkart/libs/watchkart.js
@@ -1,91 +1,132 @@
-var jsonWatch = {"titan"  :[{"name": "orion", "cost": 10000, "url": "images/titan.jpg"},{"name": "edge", "cost": 12000, "url": "images/titan.jpg"},{"name": "nebula", "cost": 8000, "url": "images/titan.jpg"},{"name": "saga", "cost": 18000, "url": "images/titan.jpg"}],
-             "tissot" :[{"name": "couturier", "cost": 25000, "url": "images/tissot.jpg"},{"name": "flamingo", "cost": 18000, "url": "images/tissot.jpg"},{"name": "touch", "cost": 22000, "url": "images/tissot.jpg"},{"name": "classic", "cost": 16000, "url": "images/tissot.jpg"}],
-             "tag"    :[{"name": "calliber", "cost": 85000, "url": "images/tag.jpg"},{"name": "carrera", "cost": 125000, "url": "images/tag.jpg"},{"name": "monaco", "cost": 400000, "url": "images/tag.jpg"}]
-           }
 $(function(){
+  var jsonWatch = {"titan"  :[{"name": "orion", "cost": 10000, "url": "images/titan.jpg"},{"name": "edge", "cost": 12000, "url": "images/titan.jpg"},{"name": "nebula", "cost": 8000, "url": "images/titan.jpg"},{"name": "saga", "cost": 18000, "url": "images/titan.jpg"}],
+                   "tissot" :[{"name": "couturier", "cost": 25000, "url": "images/tissot.jpg"},{"name": "flamingo", "cost": 18000, "url": "images/tissot.jpg"},{"name": "touch", "cost": 22000, "url": "images/tissot.jpg"},{"name": "classic", "cost": 16000, "url": "images/tissot.jpg"}],
+                   "tag"    :[{"name": "calliber", "cost": 85000, "url": "images/tag.jpg"},{"name": "carrera", "cost": 125000, "url": "images/tag.jpg"},{"name": "monaco", "cost": 400000, "url": "images/tag.jpg"}]
+                  }
   var watch = new watchKart();
-  watch.displayWatch('All');
-  $('body').delegate('#brand', 'change', function(){
-    var brand = $('#brand option:selected').val();
-    watch.displayWatch(brand);
+  var body = $('body');
+  //initially display all watches;
+  watch.displayAllWatch(jsonWatch);
+  //event handler for brand change
+  body.delegate('#brand', 'change', function(){
+    var brand = $('#brand').val();
+    watch.filterWatch(brand, jsonWatch);
   });
-  $('body').delegate('#mycartTab', 'click',function(){
-    $('#mycartTab').addClass('selected');
-    $('#productTab').removeClass('selected');
-    $('#mycartDiv').show();
-    $('#productDiv').hide();
+  //event handler for for tab clicks
+  body.delegate('#mycartTab, #productTab', 'click',function(){
+    $('.tab').removeClass('selected');
+    $(this).addClass('selected');
+    var divId = $(this).attr('id').replace('Tab', 'Div');
+    $('#' + divId + '').show()
+                       .siblings('div').hide();
   })
-  $('body').delegate('#productTab', 'click',function(){
-    $('#mycartTab').removeClass('selected');
-    $('#productTab').addClass('selected');
-    $('#mycartDiv').hide();
-    $('#productDiv').show();
+  //event handler for search button
+  body.delegate('#searchButton', 'click', function(){
+    var searchBrand = $('#search').val().trim();
+    if(searchBrand != ''){
+      watch.filterWatch(searchBrand,jsonWatch);
+    }
   })
-  $('body').delegate('#addToCart', 'click',function(){
-    var id = $(this).closest('div').attr('id');
-    watch.addToCart(id);
+  //event handler for clear button
+  body.delegate('#clearButton', 'click', function(){
+    $('#search').val("");
   })
-  $('body').delegate('.remove', 'click', function(){
+  //event handler for adding watches to cart.
+  body.delegate('.addToCart', 'click',function(){
+    var divId = $(this).closest('div').attr('id');
+    buttonobj = $(this);
+    watch.addToCart(divId, buttonobj);
+  })
+  //event handler for removing watches from cart
+  body.delegate('.remove', 'click', function(){
     var id = $(this).attr('id');
     watch.removeFromCart(id);
   })
-  $('body').delegate('#checkout', 'click', function(){
-    $('body').html("<h1>Thank you for shopping with watchKart :)</h1>")
+  //event handler for checkout buttons.
+  body.delegate('#checkout', 'click', function(){
+    $('body').html("<h1>Thank you for shopping with watchKart :)</h1><br><button id='shopAgain'>shop again</button>")
+  })
+  body.delegate('#shopAgain', 'click', function(){
+    location.reload();
+  })
+  //event handler for button buttons.
+  body.delegate('.updateRow', 'focusout', function(){
+    watch.updateRow($(this));
   })
 });
-function watchKart(){
-  var totalAmount = 0;
-  this.displayWatch = function(brand){
-    $('#productTab').addClass('selected');
-    $('#productContent div').remove();
-    if(brand == 'All'){
-      var key;
-      for (key in jsonWatch){
-        if (jsonWatch.hasOwnProperty(key)){
-          for(var i = 0; i < jsonWatch[key].length; i++){
-            var div = $('#sample').clone();
-            $(div).attr('id',key + "_" +i)
-                  .find('#spanBrand').text(" brand: " + key)
-                  .closest('div').find('img').attr('src', jsonWatch[key][i].url)
-                  .closest('div').find('#spanName').text(" watch: " + jsonWatch[key][i].name)
-                  .closest('div').find('#spanPrice').text(" Price: Rs:" + jsonWatch[key][i].cost);
-            $('#productContent').append(div);
-          }
+var watchKart = function(){
+  this.displayAllWatch =function(jsonWatch){
+    var key;
+    for (key in jsonWatch){
+      if (jsonWatch.hasOwnProperty(key)){
+        for(var i = 0; i < jsonWatch[key].length; i++){
+          var div = $('#sample').clone();
+          $(div).attr('id',key + "_" +i)
+                .find('#spanBrand').text(key)
+                .closest('div').find('img').attr('src', jsonWatch[key][i].url)
+                .closest('div').find('#spanName').text(jsonWatch[key][i].name)
+                .closest('div').find('#spanPrice').text(jsonWatch[key][i].cost);
+          $('#productContent').append(div);
         }
       }
     }
+  }
+  this.filterWatch = function(brand, jsonWatch){
+    var brand = brand.toLowerCase();
+    if(brand == 'all'){
+      $('.append').show();
+    }
     else{
-      var brand = brand.toLowerCase();
+      $('.append').hide();
       for(var i = 0; i < jsonWatch[brand].length; i++){
-        var div = $('#sample').clone();
-        $(div).attr('id',brand + "_" +i)
-              .find('#spanBrand').text(" brand: " + brand)
-              .closest('div').find('img').attr('src', jsonWatch[brand][i].url)
-              .closest('div').find('#spanName').text(" watch: " + jsonWatch[brand][i].name)
-              .closest('div').find('#spanPrice').text(" Price: Rs:" + jsonWatch[brand][i].cost);
-        $('#productContent').append(div);
+        $('#' + brand +'_' + i + '').show();
       }
-    }
+    }        
   }
-  this.addToCart = function(param){
-    id = param.split("_");
-    var product = jsonWatch[id[0]][id[1]].name;
-    var price = jsonWatch[id[0]][id[1]].cost;
-    var quantity = $('#'+ param + '').find('input').val();
+  this.addToCart = function(param, obj){
+    var product = obj.siblings('#spanName').text();
+    var price = obj.siblings('#spanPrice').text();
+    var quantity = obj.siblings('input').val();
     var subTotal = price*quantity;
-    $('#'+ param+'').find('input').val("");
+    obj.siblings('input').val("");
+    var that = this;
     if(/\d/.test(quantity)){
-      $('#myCart').append('<tr><td>' + product + '</td><td>' + price + '</td><td>' + quantity + '</td><td>' + subTotal + '</td><td><button class="remove" id="button_' + param + '">Remove</button></td></tr>');
-      totalAmount += subTotal;
-      $('#total').val(totalAmount);
-      $('#mycartTab').text('My Cart (' + ($('#myCart tr').length-1) + ')');
-      $('#button_' + param +'').data("amountToReduce",subTotal);
+      obj.siblings('input[type="text"]').attr('style', '');
+      obj.attr('disabled', 'true');
+      $('#myCart').append('<tr><td>' + product + '</td><td name="price">' + price + '</td><td><input class="updateRow" id="update_' + param + '" maxlength="1" size="1" value="' + quantity + '" /></td><td name="subTotal">' + subTotal + '</td><td><button class="remove" id="button_' + param + '">Remove</button></td></tr>');
+      this.updateCartValues();
+    }
+    else{
+      obj.siblings('input[type="text"]').attr('style', 'background-color:red;');
     }
   }
-  this.removeFromCart = function(param){
-    var amountToReduce = $('#' + param + '').data("amountToReduce");
+   this.removeFromCart = function(param){
+    divId = param.split("button_");
+    divId = divId[1];
+    $('#' + divId + '').find('button.addToCart').removeAttr('disabled');
     $('#' + param + '').closest('tr').remove();
-    totalAmount -= amountToReduce;
+    this.updateCartValues();
+  }
+  this.updateCartValues = function(){
+    var totalAmount = 0;
+    $('#mycartTab').text('My Cart (' + ($('#myCart tr').length-1) + ')');
+    $('td[name="subTotal"]').each(function(){
+      totalAmount += parseInt($(this).text());
+    })
     $('#total').val(totalAmount);
   }
-}
+  this.updateRow = function(updateId){
+    var tr = $(updateId.closest('tr'));
+    var price = tr.find('td[name="price"]').text();
+    var quantity =tr.find('input').val();
+    if(/\d/.test(quantity)){
+      var newSubTotal = price*quantity;
+      tr.find('td[name="subTotal"]').text(newSubTotal);
+      this.updateCartValues();
+    }
+    else{
+      tr.find('input').attr('style', 'background-color:red;');
+    }
+  }
+} 
+ 
